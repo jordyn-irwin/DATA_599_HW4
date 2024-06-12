5a7595e52736d11c28d1aeb5a9d3a3fe2db3af11
jordyn-irwin <147575264+jordyn-irwin@users.noreply.github.com>
2024-06-11 23:25:19 -0700

    Update reducer.py

--- docs/reducer.py
+++ docs/reducer.py
@@ -8,8 +8,7 @@ current_word = None
 current_count = 0
 word = None
 
-for line in sys.stdin: 
-    for line in sys.stdin: 
+for line in sys.stdin:  
     line = line.strip() 
     word, count = line.split('\t', 1) 
     try: 
