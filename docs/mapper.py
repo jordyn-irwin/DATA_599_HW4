3800377460468cdd72aa2528fa5b6c105b07d3b3
jordyn-irwin <147575264+jordyn-irwin@users.noreply.github.com>
2024-06-11 22:56:32 -0700

    Update mapper.py

--- docs/mapper.py
+++ docs/mapper.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # -*-coding:utf-8 -*
-import sys 
+import sys
+import re
   
 for line in sys.stdin: 
     line = line.strip() 
-    words = line.split() 
+    words = re.sub("[^A-Za-z]","", line).split() 
     for word in words:
         print('%s\t%s' % (word, 1))
